cc10c089b2579684148151f3bb256deb6d8e0d55
NikitaShmatchenko <nshm1907@gmail.com>
2020-08-06 21:38:10 +0300

    Added some data populating.

--- src/main/resources/db-schema.sql
+++ src/main/resources/db-schema.sql
@@ -4,3 +4,18 @@ CREATE TABLE KANJI(
     MEANING VARCHAR(20),
     STORY VARCHAR(500)
 );
+
+INSERT INTO KANJI (CHARACTER, MEANING, STORY)
+VALUES ('千', 'thousand', 'It takes many drops of ten to get to a thousand.');
+
+INSERT INTO KANJI (CHARACTER, MEANING, STORY)
+VALUES ('舌', 'tongue', 'A thousand mouths speak the same TONGUE.');
+
+INSERT INTO KANJI (CHARACTER, MEANING, STORY)
+VALUES ('火', 'fire', 'Flame flickers.');
+
+INSERT INTO KANJI (CHARACTER, MEANING, STORY)
+VALUES ('炎', 'inflammation', 'An inflammation can often feel like FIRE upon FIRE burning.');
+
+INSERT INTO KANJI (CHARACTER, MEANING, STORY)
+VALUES ('災', 'disaster', 'What a disaster! First a FIRE, then on top of it came FLOODs.');
